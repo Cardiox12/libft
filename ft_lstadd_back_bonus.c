2408df70ecc5ba630870ccc846130f90c1d3ba1d
Tony Bellavia <at.bellavia@gmail.com>
2019-10-12 18:57:57 +0200

    finished patching ft_lstadd_back

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -4,11 +4,13 @@ void    ft_lstadd_back(t_list **alst, t_list *new)
 {
 	t_list *current;
 
-	if (alst != NULL)
+	if (*alst != NULL)
 	{
 		current = *alst;
-		while (current->next != NULL)
+		while (current->next)
 			current = current->next;
 		current->next = new;
 	}
+	else
+		*alst = new;
 }
