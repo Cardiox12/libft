3a4bd04f1ca1062bdee2d28e00da87c54818b538
Tony Bellavia <at.bellavia@gmail.com>
2019-10-21 14:31:06 +0200

    finished editing lstmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,26 +6,32 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 13:54:48 by bbellavi          #+#    #+#             */
-/*   Updated: 2019/10/16 14:39:55 by elfamoso         ###   ########.fr       */
+/*   Updated: 2019/10/20 17:52:00 by elfamoso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list *new_list;
+	t_list *begin;
 	t_list *tmp;
 
 	if (lst != NULL)
 	{
+		tmp = ft_lstnew(f(lst->content));
+		begin = tmp;
 		while (lst)
 		{
-			tmp = ft_lstnew(f(lst->content));
 			if (tmp == NULL)
+			{
+				ft_lstiter(begin, del);
 				return (NULL);
+			}
 			ft_lstadd_back(&new_list, tmp);
 			lst = lst->next;
+			tmp = ft_lstnew(f(lst->content));
 		}
 		return (new_list);
 	}
