b5390cf6de5dc6198a1f470d96a325273f034016
Tony Bellavia <at.bellavia@gmail.com>
2019-10-30 22:52:35 +0100

    finished patching ft_lstdelone

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,7 +6,7 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 13:54:34 by bbellavi          #+#    #+#             */
-/*   Updated: 2019/10/30 17:40:27 by bbellavi         ###   ########.fr       */
+/*   Updated: 2019/10/30 22:51:54 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void    ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-    if (lst != NULL && del != NULL)
+	if (lst != NULL && del != NULL)
 	{
 		del(lst->content);
 		free(lst);
