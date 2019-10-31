da7a023081721e03293a6de76b555b91e6b34b83
Tony Bellavia <bbellavi@student.42.fr>
2019-10-31 13:37:25 +0100

    resolving norme errors

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -6,13 +6,13 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 13:54:34 by bbellavi          #+#    #+#             */
-/*   Updated: 2019/10/30 22:51:54 by bbellavi         ###   ########.fr       */
+/*   Updated: 2019/10/31 13:34:43 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    ft_lstdelone(t_list *lst, void (*del)(void *))
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (lst != NULL && del != NULL)
 	{
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,34 +6,11 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 15:39:49 by bbellavi          #+#    #+#             */
-/*   Updated: 2019/10/30 16:43:19 by bbellavi         ###   ########.fr       */
+/*   Updated: 2019/10/31 12:06:02 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-// static void	*ft_memmove(void *dst, const void *src, size_t len)
-// {
-// 	const unsigned char	*uchar_src = src;
-// 	unsigned char		tmp_dst[len];
-// 	unsigned char		*uchar_dst;
-// 	size_t				index;
-
-// 	uchar_dst = dst;
-// 	index = 0;
-// 	while (index < len)
-// 	{
-// 		tmp_dst[index] = uchar_src[index];
-// 		index++;
-// 	}
-// 	index = 0;
-// 	while (index < len)
-// 	{
-// 		uchar_dst[index] = tmp_dst[index];
-// 		index++;
-// 	}
-// 	return (uchar_dst);
-// }
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
