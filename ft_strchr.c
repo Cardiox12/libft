430ab7481c51b24a738e9990088f6b8da9d24074
Tony Bellavia <at.bellavia@gmail.com>
2019-11-05 11:38:51 +0100

    pushing

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 15:38:54 by bbellavi          #+#    #+#             */
-/*   Updated: 2019/10/22 15:38:55 by bbellavi         ###   ########.fr       */
+/*   Updated: 2019/11/04 13:27:13 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	char char_c;
+	unsigned char char_c;
 
 	char_c = c;
 	while (*s)
