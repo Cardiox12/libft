ac1735523642ccf9081ae2bb2ffbe7c8b40cc498
Tony <cardiox1212@hotmail.fr>
2020-09-17 00:07:36 +0200

    Add ft_strncpy and strndup, should add test and create separate files

--- src/ft_strings/ft_split.c
+++ src/ft_strings/ft_split.c
@@ -3,93 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tony <tony@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 15:39:01 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/04/30 10:15:50 by tony             ###   ########.fr       */
+/*   Updated: 2020/09/16 23:45:42 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_objects.h"
 #include "ft_strings.h"
 
-static size_t	ft_count_substr(const char *s, char c)
+t_string_list	*ft_split(char const *s, char c)
 {
-	size_t counter;
+	t_string_list *list;
 
-	counter = 0;
+	list = NULL;
+	string_list_create(&list);
 	while (*s)
 	{
-		if (*s != c)
+		if (ft_strchr(s, c) != NULL)
 		{
-			counter++;
-			while (*s && *s != c)
-				s++;
+			printf()
 		}
-		else
-			s++;
-	}
-	return (counter);
-}
-
-static size_t	ft_strlen_to_sep(const char *s, char c)
-{
-	const char *final_s = s;
-
-	while (*s && *s != c)
 		s++;
-	return (s - final_s);
-}
-
-static void		free_strings_array(char **strings, size_t size)
-{
-	size_t index;
-
-	index = 0;
-	while (index < size)
-		free(strings[index++]);
-	*strings = NULL;
-}
-
-static char		*ft_strndup(const char *s, size_t len)
-{
-	char *string;
-	char *begin;
-
-	string = malloc(sizeof(char) * (len + 1));
-	if (string == NULL)
-		return (NULL);
-	begin = string;
-	while (len-- != 0 && *s)
-		*string++ = *s++;
-	*string = '\0';
-	return (begin);
-}
-
-char			**ft_split(char const *s, char c)
-{
-	char	**strings;
-	size_t	index;
-
-	index = 0;
-	if ((strings = malloc(sizeof(char*) * (ft_count_substr(s, c) + 1))) == NULL)
-		return (NULL);
-	while (*s != '\0')
-	{
-		if (*s != c)
-		{
-			strings[index] = ft_strndup(s, ft_strlen_to_sep(s, c));
-			if (strings[index] == NULL)
-			{
-				free_strings_array(strings, index);
-				return (NULL);
-			}
-			while (*s && *s != c)
-				s++;
-			index++;
-		}
-		else
-			s++;
 	}
-	strings[index] = NULL;
-	return (strings);
+	return (NULL);
 }
